62b4b86115a7470777f50a1d5122b11d0bf8dccb
DiyaRajpal <81161393+DiyaRajpal@users.noreply.github.com>
2021-04-28 17:34:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,33 +5,35 @@ var mouseImg1,mouseImg2,mouseImg3,mouse;
 function preload() {
     //load the images here
     bgImg=loadImage("garden.png");
-    catImg1=loadAnimation("cat1.png");
-    mouseImg1=loadAnimation("mouse1.png");
-    catImg2=loadAnimation("cat2.png","images/cat3.png");
-    mouseImg2=loadAnimation("mouse2.png","images/mouse3.png");
-    catImg3=loadAnimation("cat4.png");
-    mouseImg3=loadAnimation("mouse4.png");
+    catImg1=loadImage("cat1.png");
+    mouseImg1=loadImage("mouse1.png");
+    catImg2=loadAnimation("cat2.png","cat3.png");
+    mouseImg2=loadAnimation("mouse2.png","mouse3.png");
+    catImg3=loadImage("cat4.png");
+    mouseImg3=loadImage("mouse4.png");
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
-    bg=createSprite(1000,800);
-    bg.addImage("background",bgImg);
-    cat=createSprite(800,400,20,20);
-    cat.addImage("catImg",catImg1);
-    mouse.createSprite(800,300,20,20)
+   // bg=createSprite(1000,800);
+    //bg.addImage("background",bgImg);
+    cat=createSprite(800,600,20,20);
+    cat.addAnimation("catImg",catImg2);
+    cat.scale=0.2;
+    mouse=createSprite(800,600,20,20)
     mouse.addImage("mouseImg",mouseImg1);
+    mouse.scale=0.2;
 }
 
 function draw() {
 
-    background(bg);
+    background(bgImg);
     //Write condition here to evalute if tom and jerry collide
   if(cat.x-mouse.x,(cat.width-mouse.width)/2){
-    mouse.addAnimation("mouseChanged",mouseImg3);  
+    mouse.addImage("mouseChanged",mouseImg3);  
     mouse.changeAnimation("mouseChanged");
-    cat.addAnimation("catChanged",catImg3);  
+    cat.addImage("catChanged",catImg3);  
     cat.changeAnimation("catChanged");
   }
     drawSprites();
@@ -43,12 +45,12 @@ function draw() {
 function keyPressed(){
 
   //For moving and changing animation write code here
-  if(keyCode==="right_arrow"){
+  if(keyCode===RIGHT_ARROW){
     mouse.addAnimation("mouseTeasing",mouseImg2); 
     mouse.changeAnimation("mouseTeasing");
     mouse.x=mouse.x+3;
  }
-  if(keyCode==="left_arrow"){
+  if(keyCode===LEFT_ARROW){
     mouse.addAnimation("mouseTeasing",mouseImg2); 
     mouse.changeAnimation("mouseTeasing");
     mouse.x=mouse.x-3;
